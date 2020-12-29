e597ed1d118e98859b691d8e91c973ec7cc47a9e
guilschaidhauer <e.guilherme.castro@gmail.com>
2020-12-28 22:03:27 -0300

    Add basic google places api call

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+const {Client} = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
+const apiKey = "1234";
+
+client
+    .elevation({
+        params: {
+            locations: [{
+                lat: 45,
+                lng: -110
+            }],
+            key: apiKey,
+        },
+        timeout: 1000, // milliseconds
+    })
+    .then((r) => {
+        console.log(r.data.results[0].elevation);
+    })
+    .catch((e) => {
+        console.log(e.response.data.error_message);
+    });
